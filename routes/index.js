d9e3e178aced410aef0f47cdc930fa31ed2113f6
Hai Nguyen <hai.nguyen@cofoundervp.com>
2020-03-05 10:01:45 +0700

    Refactor code

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ module.exports = function(sockIO, i18n) {
     var mysql = require('mysql')
     const moment = require("moment");
     require('dotenv').config({ path: 'push-server-config.env' });
+
+    console.log('run heree');
     // var connection = mysql.createConnection({
     //   host: process.env.HOST,
     //   user: process.env.USERNAME,
